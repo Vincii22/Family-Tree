eb15445e2eea85be8bfb3e34972ee1cffcecbf35
Arvin Milan <arvingm1522@gmail.com>
2025-02-24 23:44:48 +0800

    Fixed Errors

---
+++ resources/js/pages/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Welcome to Gojar Clan</h1>
+      <p>This is the landing page.</p>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: "LandingPage",
+  };
+  </script>
+
+  <style scoped>
+  h1 {
+    color: #3490dc;
+  }
+  </style>
